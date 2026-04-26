81bd41f76fdc93dfdda7c00cd1a80b1977d8dba5
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-26 22:25:16 +0300

    threads in c

--- main.c
+++ main.c
@@ -1,30 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<pthread.h>
 
-//115  c  stack and heap memory
-
-//stack temporary data
-//heap long waiting memory compare to stack
-
-
-int* heapAllocate(int size){
-    int* arry = malloc(sizeof(int)*size);
-
-printf("The address of the array %p \n",arry);
-printf("The address of the variable [arry] = %p \n",&arry);
-
-    return arry;
-}
+//117  c  threads
+// pthread_create(thread, attr, routine, arg);
+ 
+ 
 
  
+pthread_t getPid (){
+    return pthread_self();
+}
+void* display(){
+    pthread_t  thread_id = getPid();
+     printf("The ID of the thread is %p\n",(void*)thread_id);
+    printf("Hello world from thread \n");
+    printf("\n===================================================\n\n");
+   
+
+    return NULL;
+}
  
 int main() {
  
- int size =10;
+  pthread_t thread_id;
 
- int* array = heapAllocate(10);
- printf("The address of array inside main %p \n",array);
- printf("The address of the variable inside main [array] = %p \n",&array);
+  pthread_create(&thread_id,NULL,display,NULL);
+  printf("thread_id in main is %p\n",(void*)thread_id);
+
+  pthread_join(thread_id,NULL);
+
+ 
  
     return 0;
 }
+
